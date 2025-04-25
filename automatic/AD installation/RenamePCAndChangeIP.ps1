9bec6f810f62bd0d752f3b1b2072b08f3334907b
unknown <zakadmaster@gmail.com>
2025-04-25 04:26:12 -0700

    wip

--- automatic/AD installation/RenamePCAndChangeIP.ps1
+++ automatic/AD installation/RenamePCAndChangeIP.ps1
@@ -48,11 +48,11 @@ if (-not $Name) {
 if (!$NoPopup) {
     
 }
-$DomainIP = Get-UserInput -Message "Enter the preferred DNS server IP address (e.g., 192.168.1.10):" -Title "DNS Server IP Address"
-if (-not $DomainIP) {
-    Write-Host "No DNS server IP address provided. Exiting..."
-    exit
-}
+# $DomainIP = Get-UserInput -Message "Enter the preferred DNS server IP address (e.g., 192.168.1.10):" -Title "DNS Server IP Address"
+# if (-not $DomainIP) {
+#     Write-Host "No DNS server IP address provided. Exiting..."
+#     exit
+# }
 
 try {
     # Check if the server name is already set
@@ -75,29 +75,29 @@ Write-Host "Configuring the server's IP address and DNS settings..."
 # Check if the IP address already exists
 $existingIP = Get-NetIPAddress -InterfaceAlias "Ethernet" -AddressFamily IPv4 | Where-Object { $_.IPAddress -eq $ServerIP }
 
-try {
-    if ($existingIP) {
-        Write-Host "The IP address $ServerIP already exists."
-        #Set-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress $ServerIP -PrefixLength 24 -DefaultGateway $DomainIP
-    } else {
-        Write-Host "Adding the new IP address $ServerIP..."
-        New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress $ServerIP -PrefixLength 24 -DefaultGateway $DomainIP
-    }
+# try {
+#     if ($existingIP) {
+#         Write-Host "The IP address $ServerIP already exists."
+#         #Set-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress $ServerIP -PrefixLength 24 -DefaultGateway $DomainIP
+#     } else {
+#         Write-Host "Adding the new IP address $ServerIP..."
+#         New-NetIPAddress -InterfaceAlias "Ethernet" -IPAddress $ServerIP -PrefixLength 24 -DefaultGateway $DomainIP
+#     }
 
-    # Check if the DNS server is already configured
-    $currentDNS = (Get-DnsClientServerAddress -InterfaceAlias "Ethernet").ServerAddresses
-    if ($currentDNS -contains $DomainIP) {
-        Write-Host "The DNS server is already set to: $DomainIP"
-    } else {
-        Write-Host "Configuring the DNS server to: $DomainIP"
-        Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $DomainIP
-    }
-} catch {
-    Write-Host "An error occurred while configuring the IP address and DNS settings: $_"
-    exit
-}
-Write-Host "The server's IP address is set to: $ServerIP"
-Write-Host "The preferred DNS server is set to: $DomainIP"
+#     # Check if the DNS server is already configured
+#     $currentDNS = (Get-DnsClientServerAddress -InterfaceAlias "Ethernet").ServerAddresses
+#     if ($currentDNS -contains $DomainIP) {
+#         Write-Host "The DNS server is already set to: $DomainIP"
+#     } else {
+#         Write-Host "Configuring the DNS server to: $DomainIP"
+#         Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses $DomainIP
+#     }
+# } catch {
+#     Write-Host "An error occurred while configuring the IP address and DNS settings: $_"
+#     exit
+# }
+# Write-Host "The server's IP address is set to: $ServerIP"
+# Write-Host "The preferred DNS server is set to: $DomainIP"
 
 # Notify the user that the process is complete
 Write-Host "The server has been successfully renamed and its IP address and DNS settings have been configured."
